c04715d508858440c0550d19deabe592b0772504
BispoJPM <marcondesjpb@gmail.com>
2025-04-03 10:47:41 -0300

    Scanner

--- Dia1/Exercicio.java
+++ Dia1/Exercicio.java
@@ -1,6 +1,6 @@
 package Dia1;
 
-public class nome {
+public class Exercicio {
     public static void main(String[] args) {
         System.out.println("\n" + "Meu nome é João Pedro" + "\n" + "Eu tenho 23 anos" + "\n");
     }
